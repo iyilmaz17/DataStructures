9933c1f01d93bec5d491495cfe632dc34e478274
İsmail Yılmaz <81736535+Ylmaz17@users.noreply.github.com>
2023-03-15 16:55:29 +0300

    Huffman Algorithm
    Huffman Algorithm

---
+++ huffmanAlgorithm-master/README.md
@@ -0,0 +1,3 @@
+# Huffman Algorithm
+Data Structures Lesson Huffman Algorithm Assignment
+Hazırlanan proje de bir txt dosyasına kayıt edilen veri okunarak ağaç yapısını kullanmadan dizi kullanarak huffman algoritması ile kodlanmıştır.
---
+++ huffmanAlgorithm-master/src/huffman/Huffman.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package huffman;
+
+import static huffman.HuffmanDecoding.huffmanDecoding;
+import static huffman.HuffmanEncoding.characterMapping;
+import static huffman.HuffmanEncoding.huffmanEncoding;
+import static huffman.HuffmanEncoding.writeCharacterMappingTable;
+import java.io.IOException;
+
+/**
+ *
+ * @author iylmz
+ */
+// İsmail YILMAZ 
+// 02215076078
+public class Huffman {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+        // Kodlama
+    public static void main(String[] args) throws IOException {
+            
+        String input = "kodlanacakDosya.txt";
+        String output = "kodlanmisDosya.txt";
+        String characterMappingOutput = "karakterHaritasi.txt";
+
+        int[] characterFrequency = Others.frequencyTable(input);
+
+        String[] characterMappingTable = characterMapping(characterFrequency);
+        writeCharacterMappingTable(characterMappingTable, characterFrequency, characterMappingOutput);
+        
+
+        String encryptedText = huffmanEncoding(characterMappingTable, input, output, characterMappingTable, characterFrequency);
+        
+        
+        String cipher = "kodlanmisDosya.txt";
+        String decyptedText = huffmanDecoding(cipher, characterMappingTable, characterFrequency);
+        System.out.println("Şifrelenecek Dosya içeriği");
+        
+        System.out.println(decyptedText);
+        
+        System.out.println("\nŞifrelenmiş Dosya İçeriği");
+        System.out.println(encryptedText);
+        
+
+    }}
